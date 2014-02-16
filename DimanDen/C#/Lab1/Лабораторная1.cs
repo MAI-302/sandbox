d1a837c244feb8d24b4cfb6d212c581b8dc5a068
DimanDen <diman.mymail@rambler.ru>
2014-02-16 22:38:15 +0400

    Лабораторные

---
+++ DimanDen/C#/Lab1/Лабораторная1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+class Laba1Class
+{
+    static void Main()
+    {
+          Console.WriteLine("Laboratornaya rabota 1");
+          Console.ReadKey();
+    }
+}   
+
---
+++ DimanDen/Java/Lab1/Лабораторная1.java
@@ -0,0 +1,11 @@
+﻿package Laba1;
+
+
+public class Klass
+{
+     public static void main(String[] args)
+    {
+        System.out.println("Laboratornaya rabota 1");
+    }
+}
+
