1fb357b1971bd7b7c7c31f982b91959e4fef44ba
alexMAI-302 <alex71321@yandex.ru>
2014-02-16 04:50:57 -0800

    Lab1.cs

---
+++ alexMAI-302/CSharp/Lab1/Lab1.cs
@@ -0,0 +1,13 @@
+using System;
+  
+   Namespace Lab1CS
+   {
+        Class Class1
+    	  {
+	           static void main()
+        		 {
+	          	    System.Console.WriteLine("Hello World!");
+	            		System.Console.ReadKey();
+        		 }
+    	  }
+   }
