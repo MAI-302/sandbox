42096c94a7dde7413eaf5ddcd9d5eb033363540c
NMary <mashunay94@inbox.ru>
2014-02-16 21:21:58 -0800

    Create Lab_1.cs

---
+++ NMary/C/Lab_1/Lab_1.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+namespace Лаб_1
+{
+    class Lab_1
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Лабораторная работа № 1");
+            System.Console.ReadLine();
+        }
+    }
+}
