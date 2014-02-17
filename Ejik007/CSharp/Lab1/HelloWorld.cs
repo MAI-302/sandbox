b31d0eeb9b381b86ac64e033c9fe709a1affa832
Станислав <stanislavvs@bk.ru>
2014-02-17 11:49:53 -0800

    Create HelloWorld.cs

---
+++ Ejik007/CSharp/Lab1/HelloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // задержка экрана
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
