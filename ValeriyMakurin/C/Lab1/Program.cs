d5de986f3422ee4c848161f2d0dacc957cf203e2
ValeriyMakurin <valer.makurin@yandex.ru>
2014-02-23 17:51:05 +0300

    Create Program.cs

---
+++ ValeriyMakurin/C/Lab1/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Laba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Лаба1");
+            System.Console.ReadKey();
+        }
+    }
+}
