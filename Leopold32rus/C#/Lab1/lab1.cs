371b99f6087f26f6c06355687b05eb9685ec79d2
Leopold32rus <Mihail-sityonkov2009@yandex.ru>
2014-02-23 15:14:58 +0400

    laba

---
+++ Leopold32rus/C#/Lab1/lab1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Who turned out the lights?!");
+            System.Console.ReadKey();
+        }
+    }
+}
