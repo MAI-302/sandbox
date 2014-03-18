060d1a4635720285459f8b6da374dfd5897e36b2
IVAjKE <galiaf2008@rambler.ru>
2014-03-18 17:59:50 +0400

    laba0ispr

--- IVAjKE/C#/lab0/ConsoleApplication2/ConsoleApplication2/Program.cs
+++ IVAjKE/C#/lab0/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -5,26 +5,25 @@ using System.Text;
 
 namespace ConsoleApplication1
 {
-class Number1
+    class Number1
     {
-static void Main(string[] args)
- {
- Vector v1 = new Vector(15, 16);
- Vector v2 = new Vector(-7, 42);
- Vector SummaVectors = v1 + v2;
- Vector RaznostVectors = v1 - v2;
- Vector Umojenie = v1 * 5;
- Vector Delenie = v2 / 3; 
- 
-    Console.WriteLine("Первый вектор: " + v1 +
-                      "\n Второй вектор: " + v2 +
-                      "\n Сумма векторов: " + SummaVectors +
-                      "\n Разность векторов: " + RaznostVectors +
-                      "\n Умножение первого вектора на скаляр: " + Umojenie + 
-                      "\n Деление второго вектора на скаляр: "+ Delenie);
-            Console.ReadKey();
- }
-   }
+      static void Main(string[] args)
+        {
+         Vector v1 = new Vector(15, 16);
+         Vector v2 = new Vector(-7, 42);
+         Vector SummaVectors = v1 + v2;
+         Vector RaznostVectors = v1 - v2;
+         Vector Umojenie = v1 * 5;
+         Vector Delenie = v2 / 3; 
+         Console.WriteLine("Vector 1 " + v1 +
+                      "\n Vector 2: " + v2 +
+                      "\n Vector 3: " + SummaVectors +
+                      "\n Raznost Vectorov: " + RaznostVectors +
+                      "\n Umnojenie vector 1 na Skalyar: " + Umojenie + 
+                      "\n Delenie vectora 2 na skalyar: "+ Delenie);
+         Console.ReadKey();
+        }
+    }
     class Vector
     {
         private double x;
@@ -35,6 +34,7 @@ static void Main(string[] args)
             x = X;
             y = Y;
         }
+
         public double X
         {
         get
@@ -42,6 +42,7 @@ static void Main(string[] args)
             return x;
            }
         }
+
         public double Y
         {
         get
@@ -49,35 +50,41 @@ static void Main(string[] args)
              return y;
            }
         }
+
         
         public static Vector operator +(Vector V1, Vector V2)
         {
         return new Vector(V1.X + V2.X, V1.Y + V2.Y);
         }
 
+
         public static Vector operator *(Vector V1, Vector V2)
         {
         return new Vector(V1.X * V2.X, V1.Y * V2.Y);
         }
+
 
         public static Vector operator -(Vector V1, Vector V2)
         {
         return new Vector(V1.X - V2.X, V1.Y - V2.Y);
         }
 
+
         public static Vector operator *(Vector V1, double scalar)
         {
         return new Vector(V1.X * scalar, V1.Y * scalar);
         }
+
 
         public static Vector operator /(Vector V2, double scalar)
         {
         return new Vector(V2.X / scalar, V2.Y / scalar);
         }
 
+
         public override string ToString()
         {
         return "{" + X + ", " + Y + "}";
         }
-}
+    }
 }
