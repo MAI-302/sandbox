e1e19f480f776db59fbd9d3fdbb67a9145eeeab9
Станислав <stanislavvs@bk.ru>
2014-03-19 18:15:13 +0300

    Update Program.cs

--- Ejik007/CSharp/Lab0/Program.cs
+++ Ejik007/CSharp/Lab0/Program.cs
@@ -56,7 +56,7 @@ namespace laba0_2
             {
                 Generate(shift,min, max, ref array);
                 Print("Полученные данные ",i, array);
-                if (((array[2] <= array[1]) && (array[2] >= array[0])) || ((array[3] <= array[1]) && (array[3] >= array[0])))
+                if (((array[2]>=array[0])&&(array[2]<=array[1]))||((array[3]>=array[0])&&(array[3]<=array[1])))
                 {
                     intersection++;                   
                 }
