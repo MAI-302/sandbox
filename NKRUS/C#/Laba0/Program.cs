091887d3cc80fe10bd61266b6589a4b3308820f4
Никита <nik1rus2moscow3@gmail.com>
2014-03-30 20:07:24 +0400

    8 задание

---
+++ NKRUS/C#/Laba0/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SortApp
+{   //
+    // 8 задание
+    //
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ввод чисел
+            double[] nums = new double[7];
+            Console.WriteLine("Введите семь чисел");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("{0}-е число: ", i + 1);
+                nums[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            CreateSortedArray(nums);
+            Sort(nums);                
+        }
+
+        
+        // Метод сортировки
+         
+        static double[] CreateSortedArray(double[] nums)
+        {
+            double temp;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+
+            return nums; 
+        }
+        // Метод Вывода
+        static void Sort(double[] nums)
+        {
+            Console.WriteLine("Вывод отсортированного массива");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+            Console.ReadLine();
+        }
+    } 
+}
