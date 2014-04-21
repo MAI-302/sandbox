e985fd7b25c4c8fb61dba98862055e84a14b7421
Станислав <stanislavvs@bk.ru>
2014-04-21 14:27:41 +0400

    Update Program.cs

--- Ejik007/CSharp/Lab0/Program.cs
+++ Ejik007/CSharp/Lab0/Program.cs
@@ -11,10 +11,10 @@ namespace laba0_2
         public static void Generate(int shift, int min, int max, ref int a, ref int b, ref int c, ref int d)
         {
             var Random = new Random();
-            a = Random.Next(min, max) + shift;
-            b = Random.Next(min, max) + shift;
-            c = Random.Next(min, max) + shift;
-            d = Random.Next(min, max) + shift;
+            a = Random.Next(min, max);
+            b = Random.Next(min, max);
+            c = Random.Next(min, max);
+            d = Random.Next(min, max);
             if (a > b)
             {
                 a = a + b;
@@ -44,24 +44,33 @@ namespace laba0_2
 
         static void Main()
         {
-            int min = -100, max = 100, a = 0, b = 0, c = 0, d = 0, number, shift, intersection = 0;
+            int a = 0, b = 0, c = 0, d = 0, number, intersection = 0;
+            Console.WriteLine("Введите минимальный порог");
+            int min = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите максимальный порог");
+            int max = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите сдвиг");
+            int shift = Convert.ToInt16(Console.ReadLine());
+            min += shift;
+            max += shift;
+
             Console.WriteLine("введите колво повторений ");
             number = int.Parse(Console.ReadLine());
-            Console.WriteLine("введите сдвиг ");
-            shift = int.Parse(Console.ReadLine());
             for (int i = 0; i < number; i++)
             {
                 Generate(shift, min, max, ref a, ref b, ref c, ref d);
                 Print("Полученные данные ", i, a, b, c, d);
-                if (((c >= a) && (c <= b)) || ((d >= a) && (d <= b)) ||
-((a >= c) && (a <= d)) || ((b >= c) && (b <= d)))
+                if (((a <= c) && (c <= b)) || 
+                    ((a <= d) && (d <= b)) ||
+                    ((c <= a) && (a <= d)) || 
+                    ((c <= b) && (b <= d)))
                 {
                     intersection++;
                 }
-                Console.Write("Кол-во пересечений ");
-                Console.WriteLine(intersection);
-                Console.ReadLine();
+                Console.WriteLine();
             }
+            Console.Write("Кол-во пересечений ");
+            Console.WriteLine(intersection);
             Console.ReadLine();
         }
     }
