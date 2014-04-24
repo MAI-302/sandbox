3df17d150a6a82ed754f292eb42a0b08181c13ac
Никита <nik1rus2moscow3@gmail.com>
2014-04-24 23:36:08 +0400

    laba0 remake

--- NKRUS/Java/src/Class1.java
+++ NKRUS/Java/src/Class1.java
@@ -1,36 +1,74 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+
 import java.util.Arrays;
 
 /**
  * Created by NKRUS on 24.04.14.
  */
 public class Class1 {
-    static double[] newnums = new double[7];
-    static double[] nums = {2,7,9,4,15,1,8};
+
     public static void main(String[] args) throws Exception{
-        //инициализируем массивы
 
+        double[] nums = {2,7,9,4,15,1,8};
+        double[] newnums = new double[7];
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); // объект для чтения входных данных
 
         System.out.println("----------- Элементы исходного массива -----------------");
-        System.out.println(Arrays.toString(nums)); //выводим массив
+        for (int i = 0; i<nums.length; ++i){
+        System.out.print(" " + nums[i]);
+        }
+
+        newnums = CreateSortedArray(nums);
         System.out.println("----------- Элементы массива после сортировки методом CreateSortedArray -----------------");
-        System.out.println(Arrays.toString(CreateSortedArray(nums))); //выводим массив
-        System.out.println("----------- Элементы массива после сортировки методом Sort -----------------");
+        for (int i = 0; i<newnums.length; ++i){
+            System.out.print(" " + newnums[i]);
+        }
+
         Sort(nums);
-        System.out.println(Arrays.toString(nums)); //выводим массив
+        System.out.println("----------- Элементы массива после сортировки методом Sort -----------------");
+        for (int i = 0; i<nums.length; ++i){
+            System.out.print(" " + nums[i]);
+        }
+
 
     }
 
     static void Sort(double[] a){
-        Arrays.sort(a);
+
+        for (int i = 0; i < a.length; i++)
+        {
+            for (int j = i + 1; j < a.length; j++)
+            {
+                if (a[i] > a[j])
+                {
+                    double temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+
     }
 
+
     static  double[] CreateSortedArray(double[] a){
-        System.arraycopy(nums, 0, a, 0, nums.length);
-        Arrays.sort(a);
-        return a;
+        double[] newnums = new double[a.length];
+        for (int i = 0; i < a.length; i++){
+            newnums[i] = a[i];
+        }
+
+        for (int i = 0; i < newnums.length; i++)
+        {
+            for (int j = i + 1; j < newnums.length; j++)
+            {
+                if (newnums[i] > newnums[j])
+                {
+                    double temp = newnums[i];
+                    newnums[i] = newnums[j];
+                    newnums[j] = temp;
+                }
+            }
+        }
+
+        return newnums;
     }
 }
