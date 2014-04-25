94a86a4fddca8eaddf6fb6a1c2e95debd853c9bf
Никита <nik1rus2moscow3@gmail.com>
2014-04-25 17:02:10 +0400

    laba0 remake3

--- NKRUS/Java/src/Class1.java
+++ NKRUS/Java/src/Class1.java
@@ -6,40 +6,37 @@ import java.util.Arrays;
  */
 public class Class1 {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
-        double[] nums = {2,7,9,4,15,1,8};
+        double[] nums = {2, 7, 9, 4, 15, 1, 8};
         double[] newnums = new double[7];
 
 
         System.out.println("----------- Элементы исходного массива -----------------");
-        for (int i = 0; i<nums.length; ++i){
-        System.out.print(" " + nums[i]);
+        for (int i = 0; i < nums.length; ++i) {
+            System.out.print(" " + nums[i]);
         }
 
         newnums = CreateSortedArray(nums);
         System.out.println("----------- Элементы массива после сортировки методом CreateSortedArray -----------------");
-        for (int i = 0; i<newnums.length; ++i){
+        for (int i = 0; i < newnums.length; ++i) {
             System.out.print(" " + newnums[i]);
         }
 
         Sort(nums);
         System.out.println("----------- Элементы массива после сортировки методом Sort -----------------");
-        for (int i = 0; i<nums.length; ++i){
+        for (int i = 0; i < nums.length; ++i) {
             System.out.print(" " + nums[i]);
         }
 
 
     }
 
-    static void Sort(double[] a){
+    static void Sort(double[] a) {
 
-        for (int i = 0; i < a.length; i++)
-        {
-            for (int j = i + 1; j < a.length; j++)
-            {
-                if (a[i] > a[j])
-                {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
                     double temp = a[i];
                     a[i] = a[j];
                     a[j] = temp;
@@ -50,14 +47,12 @@ public class Class1 {
     }
 
 
-    static  double[] CreateSortedArray(double[] a){
+    static double[] CreateSortedArray(double[] a) {
         double[] newnums = new double[a.length];
-        for (int i = 0; i < a.length; i++){
+        for (int i = 0; i < a.length; i++) {
             newnums[i] = a[i];
         }
-
         Sort(newnums);
-
         return newnums;
     }
 }
