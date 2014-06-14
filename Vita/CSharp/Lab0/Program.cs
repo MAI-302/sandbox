a641b13253b68d350933de6d1716314bc7888757
VitalinaEvdokimova <vita.evd@gmail.com>
2014-06-14 22:03:10 +0400

    Update Program.cs

--- Vita/CSharp/Lab0/Program.cs
+++ Vita/CSharp/Lab0/Program.cs
@@ -7,82 +7,50 @@ namespace zadanie2
 {
     class Program
     {
-        public enum level { level0, level1, level2 };
-        /// <summary>Показывает пункты меню, для 3 уровненей</summary>
-        /// <param name="l">Перечислитель уровеней</param>
-        public static void ShowLevel(level l) {
+        public enum level { level0, level1, level2, str, exit };
+
+        /// <summary>Отображает меню с заданным уровнем</summary
+        /// <param name="l">Уровень для отображения</param>
+        private static void ShowMenu(level l) {
+            Console.Clear(); 
             switch (l) {
                 case level.level0:
                     Console.WriteLine("************* Уровень 0 **************");
-                    Console.WriteLine("Переход на уровень 1 --> 1");
-                    Console.WriteLine("Выход                --> 0");
-                    break;
+                    Console.WriteLine("Переход на уровень 1 --> 1\nВыход                --> 0");
+                    Levels(l, level.level1, level.exit);    break;
                 case level.level1:
                     Console.Clear();
                     Console.WriteLine("************* Уровень 1 **************");
-                    Console.WriteLine("Переход на уровень 2 --> 1");
-                    Console.WriteLine("Переход на уровень 0 --> 0");
-                    break;
+                    Console.WriteLine("Переход на уровень 2 --> 1\nПереход на уровень 0 --> 0");
+                    Levels(l, level.level2, level.level0);    break;
                 case level.level2:
                     Console.Clear();
                     Console.WriteLine("************* Уровень 2 **************");
-                    Console.WriteLine("Переход на уровень 1 --> 1");
-                    Console.WriteLine("Вывести строку на экран --> 2");
-                    break;
+                    Console.WriteLine("Переход на уровень 1 --> 1\nВывести строку на экран --> 0");
+                    Levels(l, level.level1, level.str);    break;
+                case level.str:
+                    Console.WriteLine("Тестовая строка");
+                    Console.WriteLine("Вернуться к Уровню 2 --> 1");
+                    Levels(l, level.level2, level.level2);    break;
+                case level.exit: break;
+                default: Console.WriteLine("Уровень не существует"); break;
             }
+        }
+        /// <summary>Уровни</summary>
+        /// <param name="l">Текущий уровень</param>
+        /// <param name="l1">Следующий уровень</param>
+        /// <param name="l2">Предыдущий уровень, или тот же или строка</param>
+        private static void Levels(level l, level l1, level l2){
+            ConsoleKeyInfo cki = new ConsoleKeyInfo();
+            cki = Console.ReadKey();    // Считываем нажатую клавишу
+            if (cki.Key != ConsoleKey.D0 && cki.Key != ConsoleKey.D1) ShowMenu(l);
+            if (cki.Key == ConsoleKey.D1) ShowMenu(l1);
+            else if (cki.Key == ConsoleKey.D0) ShowMenu(l2);
         }
 
         static void Main(string[] args) {
-            ConsoleKeyInfo cki;     // Информация о нажатой клавише
-            bool f = true;
-            do {
-                ShowLevel(level.level0);        // Показываем нулевой уровень
-                cki = Console.ReadKey();        // Считываем (нажатую клавишу) выбор пользователя
-                do {
-                    // Если клавиша 1
-                    if (cki.Key == ConsoleKey.D1) {
-                        ShowLevel(level.level1);    // Показываем первый уровень
-                        cki = Console.ReadKey();    // Считываем (нажатую клавишу) выбор пользователя
-                        // Если нажатая клавиша 1
-                        if (cki.Key == ConsoleKey.D1) {
-                            f = true;      // флаг выхода из уровня
-                            do {
-                                ShowLevel(level.level2);    // Показываем второй уровень
-                                cki = Console.ReadKey();    // Считываем (нажатую клавишу) выбор пользователя
-                                // Если нажатая клавиша 1
-                                if (cki.Key == ConsoleKey.D1) {
-                                    // Чистим консоль и переходим на уровень 1
-                                    Console.Clear(); f = false;
-                                }
-                                // иначе если была нажата клавиша 2
-                                else if (cki.Key == ConsoleKey.D2) {
-                                    // Чистим консоль и остаемся на том же уровне
-                                    Console.Clear();
-                                    // Выводим строку
-                                    Console.WriteLine("Эта строка находится на уровне 2");
-                                    Console.WriteLine("Вернуться к Уровню 2 --> 1");
-                                    cki = Console.ReadKey();
-                                }
-                                else {
-                                    Console.Clear();
-                                }
-                            } while (f);    // Пока не пожелаем вернуться на уровень 1
-                            f = true;
-                        }
-                        // иначе если была нажата клавиша 0, 
-                        else if (cki.Key == ConsoleKey.D0) {
-                            // чистим консоль и переходим на уровень 0
-                            Console.Clear(); f = false; cki = new ConsoleKeyInfo();
-                        }
-                        else {
-                            Console.Clear();
-                        }
-                    }
-                    else {
-                        Console.Clear();
-                    }
-                } while (f); 
-            } while (cki.Key != ConsoleKey.D0);   // Выводим пункт до тех пор пока не нажали 0
+            // Отображаем меню с нулевого уровня
+            ShowMenu(level.level0);
         }
     }
 }
