e9d161756b332333fe83d9539e9396a124c5da78
Никита <nik1rus2moscow3@gmail.com>
2014-06-20 15:38:23 +0400

    Update Lab0

--- NKRUS/Java/src/Class1.java
+++ NKRUS/Java/src/Class1.java
@@ -13,26 +13,21 @@ public class Class1 {
 
 
         System.out.println("----------- Элементы исходного массива -----------------");
-        for (int i = 0; i < nums.length; ++i) {
-            System.out.print(" " + nums[i]);
-        }
+        PrintScreen(nums);
 
         newnums = CreateSortedArray(nums);
         System.out.println("----------- Элементы массива после сортировки методом CreateSortedArray -----------------");
-        for (int i = 0; i < newnums.length; ++i) {
-            System.out.print(" " + newnums[i]);
-        }
+        PrintScreen(newnums);
 
         Sort(nums);
         System.out.println("----------- Элементы массива после сортировки методом Sort -----------------");
-        for (int i = 0; i < nums.length; ++i) {
-            System.out.print(" " + nums[i]);
-        }
+        PrintScreen(nums);
+
 
 
     }
 
-    static void Sort(double[] a) {
+    private static void Sort(double[] a) {
 
         for (int i = 0; i < a.length; i++) {
             for (int j = i + 1; j < a.length; j++) {
@@ -47,12 +42,19 @@ public class Class1 {
     }
 
 
-    static double[] CreateSortedArray(double[] a) {
+    private static double[] CreateSortedArray(double[] a) {
         double[] newnums = new double[a.length];
         for (int i = 0; i < a.length; i++) {
             newnums[i] = a[i];
         }
         Sort(newnums);
         return newnums;
+    }
+
+    private static void PrintScreen(double[] a){
+        for (int i = 0; i < a.length; ++i) {
+            System.out.print(" " + a[i]);
+
+    }
     }
 }
